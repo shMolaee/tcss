5eed3013de87d3e1139c27d5fd33afa9b024c494
iman-molaeie <82046734+iman-molaeie@users.noreply.github.com>
2022-04-11 11:14:48 -0700

    3 page aded

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ const swiper = new Swiper(".swiper", {
     },
 });
 
-documents.querySelector(".menu-oppener > a").onclick = () => {
-    documents.querySelector(".menu-oppener > ul").classlist.remove("hidden");
-}
+const otpInputs = document.querySelectorAll("#otp input");
+
+otpInputs.forEach((e,i) => {e.onkeyup = (er) => {
+        if (er.code == "Backspace" && i < otpInputs.length - 1) {
+            otpInputs[i+1].focus()
+            otpInputs[i+1].select()
+        }
+        else if (i >= 1 && er.code != "Backspace") {
+            otpInputs[i-1].focus()
+            otpInputs[i-1].select()
+        }
+    }
+
+})
